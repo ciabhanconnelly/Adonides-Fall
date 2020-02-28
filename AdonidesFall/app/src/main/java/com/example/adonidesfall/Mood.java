020f91990643da8f8ddf46aa92a40c014b6c7856
Connelly <ciaconne@iu.edu>
2020-02-28 12:06:25 -0500

    Signed-off-by: Connelly <ciaconne@iu.edu>update files

---
+++ AdonidesFall/app/src/main/java/com/example/adonidesfall/Mood.java
@@ -0,0 +1,43 @@
+package com.example.adonidesfall;
+
+public class Mood {
+    private int happySad;
+    private int passivePushy;
+    private int excitedCalm;
+    private int truth;
+
+    public int getHappySad() {
+        return happySad;
+    }
+
+    public void setHappySad(int happySad) {
+        this.happySad = happySad;
+    }
+
+    public int getPassivePushy() {
+        return passivePushy;
+    }
+
+    public void setPassivePushy(int passivePushy) {
+        this.passivePushy = passivePushy;
+    }
+
+    public int getExcitedCalm() {
+        return excitedCalm;
+    }
+
+    public void setExcitedCalm(int excitedCalm) {
+        this.excitedCalm = excitedCalm;
+    }
+
+    public int getTruth() {
+        return truth;
+    }
+
+    public void setTruth(int truth) {
+        this.truth = truth;
+    }
+
+
+
+}
